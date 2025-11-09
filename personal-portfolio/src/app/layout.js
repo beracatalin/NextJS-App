d27cc4194ed9e21eb2190ca8c9bde2454676616d
beracatalin <catalin_bera@yahoo.com>
2025-11-09 17:17:58 +0200

    SEO

--- personal-portfolio/src/app/layout.js
+++ personal-portfolio/src/app/layout.js
@@ -1,7 +1,21 @@
 import Navbar from "@/components/Navbar";
 import ThemeProvider from "../theme/theme-provider";
 import "./globals.css";
-
+export const metadata = {
+  title: "Cata Space | Personal Portfolio in NextJS",
+  description: "Explore my Portfolio, with projects in AngularJS, React, NextJS, UI/UX",
+  keywords: ["Web Developer", "React", "Next.js", "TailwindCSS", "Portfolio", "UI/UX"],
+  authors: [{ name: "Catalin" }],
+  openGraph: {
+    title: "Catalin Portfolio",
+    description: "Check out my projects and skills in web development and design.",
+    url: "https://cata-space.vercel.app/",
+    siteName: "Cata Space",
+    images: [{ url: "/me.jpg", width: 1200, height: 630 }], //imagina care apare cand dai pe social media
+    locale: "en_US",
+    type: "website",
+  },
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
